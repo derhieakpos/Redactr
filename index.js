2732e894b5cfea0d1d75525d33528a49967d9507
derhieakpos <derhieakpos@gmail.com>
2023-11-09 03:31:26 +0100

    Basic Redact function done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // alert("Hello World!")
-let text = document.getElementById("text");
-if (text){
-    alert(text);
-}
+const input = document.getElementById('input');
+const scrambled = document.getElementById('scrambled');
+const redactedText = document.getElementById('redacted_text');
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const redactedContent = input.value.replace(new RegExp(scrambled.value, 'g'), '****');
+  redactedText.innerHTML = redactedContent;
+});
