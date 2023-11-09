d605ab82d386b67a588af856f68f41617f696d82
derhieakpos <derhieakpos@gmail.com>
2023-11-09 08:32:30 +0100

    Redact and Width update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const scrambled = document.getElementById('scrambled');
 const redactedText = document.getElementById('redacted_text');
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  const redactedContent = input.value.replace(new RegExp(scrambled.value, 'g'), '****');
+  const redactedContent = input.value.replace(new RegExp(scrambled.value, 'g'), '*');
   redactedText.innerHTML = redactedContent;
 });
